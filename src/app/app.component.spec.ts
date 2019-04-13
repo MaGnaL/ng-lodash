987a9e3a82ecfd3191c33c5bdbf81c3cb661c23c
Manuel Lang <code@manuel-lang.dev>
2019-04-13 19:28:13 +0200

    chore: rename ng-lodash to ng-lodash-demo

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ng-lodash'`, () => {
+  it(`should have as title 'ng-lodash-demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('ng-lodash');
+    expect(app.title).toEqual('ng-lodash-demo');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-lodash!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to ng-lodash-demo!');
   });
 });
--- src/karma.conf.js
+++ src/karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/ng-lodash'),
+      dir: require('path').join(__dirname, '../coverage/ng-lodash-demo'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
